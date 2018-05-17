c513eeb1ba0f84714294cd1e2a91647b21bc5893
REDO <leeduyoung2002@gmail.com>
2018-05-17 20:54:07 +0900

    Update config.js

--- functions/lib/src/config/config.js
+++ functions/lib/src/config/config.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.firebaseConfig = {
-    apiKey: "AIzaSyDFKnXwNxviBI2H-1Vp8-U6XxTzLShLnoQ",
-    authDomain: "today-todo-app.firebaseapp.com",
-    databaseURL: "https://today-todo-app.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "today-todo-app",
-    storageBucket: "today-todo-app.appspot.com",
-    messagingSenderId: "944951810488"
+    storageBucket: "",
+    messagingSenderId: ""
 };
 //# sourceMappingURL=config.js.map
